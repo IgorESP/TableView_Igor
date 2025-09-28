77687d6dbf668293fa877702f9396adaf3c2e4ab
IgorESP <igor.esparza2@gmail.com>
2025-09-28 18:45:20 +0200

    FIX: Intento sin exito de conseguir el jar ejecutable despues de demasiadas pruebas

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.igoresparza.controladores to javafx.fxml;
-    opens com.igoresparza.modelo to javafx.base;
-
-    exports com.igoresparza; // <-- exportamos tu paquete principal
-}
