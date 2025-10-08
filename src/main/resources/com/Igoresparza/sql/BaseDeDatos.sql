2677ac35a6b038c0cde84a28732c691ffa8f095e
IgorESP <igor.esparza2@gmail.com>
2025-10-08 16:39:10 +0200

    FIX: Cmabios sql arreglados

--- src/main/resources/com/Igoresparza/sql/BaseDeDatos.sql
+++ src/main/resources/com/Igoresparza/sql/BaseDeDatos.sql
@@ -1,7 +1,11 @@
-CREATE TABLE persona (
+CREATE TABLE IF NOT EXISTS PERSONA (
         person_id INT NOT NULL AUTO_INCREMENT,
         first_name VARCHAR(100) NOT NULL,
         last_name VARCHAR(100) NOT NULL,
         birth_date DATE,
         PRIMARY KEY (person_id)
 );
+
+insert into PERSONA (person_id, first_name, last_name, birth_date)
+values ("1","Ruben","Albeniz",'02-02-2001'),
+       ("2","Alberto","García",'02-01-1999');
